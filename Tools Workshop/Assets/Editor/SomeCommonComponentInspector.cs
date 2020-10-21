2071df16ecc7de79a9ff90861aac2f65a5f1c01f
Leithianîn <m.lukas@rubika-edu.com>
2020-10-21 12:46:03 +0200

    Serialized property Next()

---
+++ Tools Workshop/Assets/Editor/SomeCommonComponentInspector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SomeCommonComponent))]
+public class SomeCommonComponentInspector : Editor
+{
+    SerializedProperty myProp;
+    SerializedProperty forkProp;
+
+    private void OnEnable()
+    {
+        myProp = serializedObject.GetIterator();
+        /*myProp.NextVisible(true);
+        Debug.Log(myProp.name);
+        Debug.Log(myProp.displayName);
+        Debug.Log(myProp.type);*/
+
+        while (myProp.NextVisible(true))
+        {
+            Debug.Log(myProp.name);
+            Debug.Log(myProp.displayName);
+            Debug.Log(myProp.type);
+
+            if (myProp.type == "Color") forkProp = myProp.Copy();
+        }
+        myProp.Reset();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        base.OnInspectorGUI();
+
+        sw.Stop();
+        EditorGUILayout.LabelField(sw.ElapsedMilliseconds.ToString());
+    }
+}
---
+++ Tools Workshop/Assets/SomeCommonComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeCommonComponent : MonoBehaviour
+{
+    //Iterator <-- GetIterator arrive là et Next va prendre les enfants
+        public float myFloat;
+        public bool myBool;
+        public Bounds myBounds;
+        public Rect myRect;
+                    //myRect.x
+                    //myRect.y
+                    //myRect.z ...
+        public AnimationCurve myCurve;
+        public Color myColor;
+        public string myString;
+        public Vector3 myVector;
+        public Object myObject;             //PPtr<$Object>
+        public AudioSource myAudioSrc;      //PPtr<$AudioSource>
+        public Gradient myGradient;
+}
